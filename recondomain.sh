7cca9516d91dd5dc45d96753222506adc20ea111
William F <48420172+willkj@users.noreply.github.com>
2019-06-24 12:47:39 -0300

    Add files via upload

---
+++ recondomain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for arquivo in $(cat lista.txt)
+do
+result =$(find
+for palavra in $(cat lista.txt)
+do
+resposta=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra/)
+if [ $resposta == "200" ]
+then
+echo "Diretorio encontrado: $palavra"
+fi
+done
+
